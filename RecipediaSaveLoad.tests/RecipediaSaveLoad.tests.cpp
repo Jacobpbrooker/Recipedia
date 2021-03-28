d748f4b3a9387a00fbbeb914568e4008594010d7
Jacob Brooker <jakepb@hotmail.com>
2021-03-28 14:21:04 -0400

    saveload tests completed, ready for other module tests to be added

--- RecipediaSaveLoad.tests/RecipediaSaveLoad.tests.cpp
+++ RecipediaSaveLoad.tests/RecipediaSaveLoad.tests.cpp
@@ -29,17 +29,96 @@ namespace RecipediaSaveLoadtests
 			Assert::AreEqual(testReturn, expectedReturn);
 		}
 	};
-	TEST_CLASS(restaurantFunctions)
+	TEST_CLASS(queueFunctions)
 	{
 	public:
-		TEST_METHOD(T01_restaurant_returnAdminConfirmation_Return1)
+		TEST_METHOD(T01_queue_enqueueIngredient_Return1)
 		{
 			// arrange
-			unsigned char x[9] = "10011001";
-			int y = 8;
+			PINGREDIENTQUEUE ingredientQueue = initializeIngredientQueue();
+			
+			FILE* fp;
+			errno_t fileError = fopen_s(&fp, "../RecipediaSaveLoad.tests/restaurants/noma/nomaingredients.txt", "r");
+
+			char* ingredient = (char*)malloc(MAXSTRINGLENGTH);
+			memset(ingredient, '\0', sizeof(char) * MAXSTRINGLENGTH);
+
+			char* unitOfMeasurement = (char*)malloc(MAXSTRINGLENGTH);
+			memset(unitOfMeasurement, '\0', sizeof(char) * MAXSTRINGLENGTH);
+
+			char* measurementString = (char*)malloc(sizeof(char) * MAXSTRINGLENGTH);
+			memset(measurementString, '\0', sizeof(char) * MAXSTRINGLENGTH);
+
+			fgets(ingredient, MAXSTRINGLENGTH, fp);
+			float measurement = atof(fgets(measurementString, MAXSTRINGLENGTH, fp));
+			fgets(unitOfMeasurement, MAXSTRINGLENGTH, fp);
+
+
+			// this will add the terminator to each of the char strings added
+			for (unsigned int i = 0; i < strlen(ingredient); i++)
+			{
+				if (ingredient[i] == '\n')
+				{
+					ingredient[i] = '\0';
+				}
+			}
+
+			for (unsigned int i = 0; i < strlen(unitOfMeasurement); i++)
+			{
+				if (unitOfMeasurement[i] == '\n')
+				{
+					unitOfMeasurement[i] = '\0';
+				}
+			}
+
 			int expectedReturn = 1;
 			// act
-			int testReturn = returnAdminConfirmation(x, y);
+			int testReturn = enqueueIngredient(ingredientQueue, createIngredient(ingredient, measurement, unitOfMeasurement));
+			// assert
+			Assert::AreEqual(testReturn, expectedReturn);
+		}
+		TEST_METHOD(T02_queue_enqueueInstruction_Return1)
+		{
+			// arrange
+			PINGREDIENTQUEUE ingredientQueue = initializeIngredientQueue();
+
+			FILE* fp;
+			errno_t fileError = fopen_s(&fp, "../RecipediaSaveLoad.tests/restaurants/noma/nomainstructions.txt", "r");
+
+			char* ingredient = (char*)malloc(MAXSTRINGLENGTH);
+			memset(ingredient, '\0', sizeof(char) * MAXSTRINGLENGTH);
+
+			char* unitOfMeasurement = (char*)malloc(MAXSTRINGLENGTH);
+			memset(unitOfMeasurement, '\0', sizeof(char) * MAXSTRINGLENGTH);
+
+			char* measurementString = (char*)malloc(sizeof(char) * MAXSTRINGLENGTH);
+			memset(measurementString, '\0', sizeof(char) * MAXSTRINGLENGTH);
+
+			fgets(ingredient, MAXSTRINGLENGTH, fp);
+			float measurement = atof(fgets(measurementString, MAXSTRINGLENGTH, fp));
+			fgets(unitOfMeasurement, MAXSTRINGLENGTH, fp);
+
+
+			// this will add the terminator to each of the char strings added
+			for (unsigned int i = 0; i < strlen(ingredient); i++)
+			{
+				if (ingredient[i] == '\n')
+				{
+					ingredient[i] = '\0';
+				}
+			}
+
+			for (unsigned int i = 0; i < strlen(unitOfMeasurement); i++)
+			{
+				if (unitOfMeasurement[i] == '\n')
+				{
+					unitOfMeasurement[i] = '\0';
+				}
+			}
+
+			int expectedReturn = 1;
+			// act
+			int testReturn = enqueueIngredient(ingredientQueue, createIngredient(ingredient, measurement, unitOfMeasurement));
 			// assert
 			Assert::AreEqual(testReturn, expectedReturn);
 		}
@@ -47,11 +126,9 @@ namespace RecipediaSaveLoadtests
 	TEST_CLASS(SaveLoadFunctions)
 	{
 	public:
-		TEST_METHOD(T01_saveFile_Return1)
+		TEST_METHOD(T01_saveLoad_saveFile_Return1)
 		{
 			// arrange
-			// open notepad++ and edit every single link to be the full file path as done above
-			//PRESTAURANTNODE X = (PRESTAURANTNODE)malloc(sizeof(RESTAURANTNODE));
 			PRESTAURANTNODE x = loadRestaurants(TESTDIRECTORY);
 			int expectedReturn = 1;
 			// act
