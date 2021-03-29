62dd9d87824202713cfa05e87e40a810a3ca27c7
mason <mward7091@conestogac.on.ca>
2021-03-29 09:22:38 -0400

    added tests for searchRecipe and searchRestaurant function

--- Recipedia.Tests/Recipedia.Tests.cpp
+++ Recipedia.Tests/Recipedia.Tests.cpp
@@ -59,5 +59,97 @@ namespace RecipediaTests
 		}
 	};
 
+	TEST_CLASS(SearchRestaurantTests)
+	{
+	public:
 
+		TEST_METHOD(T004_maidotestname3)
+		{
+			//Arrange
+			char* input = "maidotestname3";
+			char* expected = "maidotestname3";
+			PRESTAURANTNODE	list = testLoad("restaurants/toptenrestaurants.txt");
+
+			//Act
+			PRESTAURANTNODE p = searchRestaurant(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+
+		TEST_METHOD(T005_geraniumtestname3)
+		{
+			//Arrange
+			char* input = "geraniumtestname3";
+			char* expected = "geraniumtestname3";
+			PRESTAURANTNODE	list = testLoad("restaurants/toptenrestaurants.txt");
+
+			//Act
+			PRESTAURANTNODE p = searchRestaurant(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+
+		TEST_METHOD(T006_mirazurtestname3)
+		{
+			//Arrange
+			char* input = "mirazurtestname3";
+			char* expected = "mirazurtestname3";
+			PRESTAURANTNODE	list = testLoad("restaurants/toptenrestaurants.txt");
+
+			//Act
+			PRESTAURANTNODE p = searchRestaurant(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+	};
+
+	TEST_CLASS(SearchRecipeTests)
+	{
+	public:
+
+		TEST_METHOD(T007_maidoribs3)
+		{
+			//Arrange
+			char* input = "maidoribs3";
+			char* expected = "maidoribs3";
+			PRESTAURANTNODE	list = testLoad("restaurants/toptenrestaurants.txt");
+
+			//Act
+			PRESTAURANTNODE p = searchRecipe(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.specialityMenu[0]);
+		}
+
+		TEST_METHOD(T008_centralmashedpotatoes3)
+		{
+			//Arrange
+			char* input = "centralmashed potatoes3";
+			char* expected = "centralmashed potatoes3";
+			PRESTAURANTNODE	list = testLoad("restaurants/toptenrestaurants.txt");
+
+			//Act
+			PRESTAURANTNODE p = searchRecipe(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.specialityMenu[1]);
+		}
+
+		TEST_METHOD(T009_mirazurcoffee3)
+		{
+			//Arrange
+			char* input = "mirazurcoffee3";
+			char* expected = "mirazurcoffee3";
+			PRESTAURANTNODE	list = testLoad("restaurants/toptenrestaurants.txt");
+
+			//Act
+			PRESTAURANTNODE p = searchRecipe(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.specialityMenu[2]);
+		}
+	};
 }
