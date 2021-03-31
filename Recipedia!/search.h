82c4ca9d386df56253795cdaf6ec68a532a39f8a
mason <mward7091@conestogac.on.ca>
2021-03-31 15:12:59 -0400

    added search tests to test project

--- Recipedia!/search.h
+++ Recipedia!/search.h
@@ -2,6 +2,6 @@
 #include "restaurant.h"
 #include "SaveLoad.h"
 
-PRESTAURANTNODE searchNum(PRESTAURANTNODE pr, int i);
-PRESTAURANTNODE searchRestaurant(PRESTAURANTNODE pr, char* i);
-PRESTAURANTNODE searchRecipe(PRESTAURANTNODE pr, char* i);
+PRESTAURANTNODE searchNum(PRESTAURANTNODE, int);
+PRESTAURANTNODE searchRestaurant(PRESTAURANTNODE, char*);
+PRESTAURANTNODE searchRecipe(PRESTAURANTNODE, char*);
--- RecipediaSaveLoad.tests/RecipediaSaveLoad.tests.cpp
+++ RecipediaSaveLoad.tests/RecipediaSaveLoad.tests.cpp
@@ -6,6 +6,7 @@ extern "C" {
 #include "queue.h"
 #include "restaurant.h"
 #include "SaveLoad.h"
+#include "search.h"
 }
 
 #define TESTDIRECTORY "../RecipediaSaveLoad.tests/restaurants/toptenrestaurants.txt"
@@ -135,6 +136,146 @@ namespace RecipediaSaveLoadtests
 			int testReturn = saveFile(x, TESTDIRECTORY);;
 			// assert
 			Assert::AreEqual(testReturn, expectedReturn);
+		}
+	};
+	TEST_CLASS(SearchNumTests)
+	{
+	public:
+
+		TEST_METHOD(T001_Five)
+		{
+			//Arrange
+			int input = 5;
+			char* expected = "centraltestname3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchNum(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+
+		TEST_METHOD(T002_One)
+		{
+			//Arrange
+			int input = 1;
+			char* expected = "maidotestname3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchNum(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+
+		TEST_METHOD(T003_Ten)
+		{
+			//Arrange
+			int input = 1;
+			char* expected = "mirazurtestname3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchNum(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+	};
+
+	TEST_CLASS(SearchRestaurantTests)
+	{
+	public:
+
+		TEST_METHOD(T004_maidotestname3)
+		{
+			//Arrange
+			char* input = "maidotestname3";
+			char* expected = "maidotestname3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchRestaurant(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+
+		TEST_METHOD(T005_geraniumtestname3)
+		{
+			//Arrange
+			char* input = "geraniumtestname3";
+			char* expected = "geraniumtestname3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchRestaurant(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+
+		TEST_METHOD(T006_mirazurtestname3)
+		{
+			//Arrange
+			char* input = "mirazurtestname3";
+			char* expected = "mirazurtestname3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchRestaurant(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.restaurantName);
+		}
+	};
+
+	TEST_CLASS(SearchRecipeTests)
+	{
+	public:
+
+		TEST_METHOD(T007_maidoribs3)
+		{
+			//Arrange
+			char* input = "maidoribs3";
+			char* expected = "maidoribs3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchRecipe(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.specialityMenu[0]);
+		}
+
+		TEST_METHOD(T008_centralmashedpotatoes3)
+		{
+			//Arrange
+			char* input = "centralmashed potatoes3";
+			char* expected = "centralmashed potatoes3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchRecipe(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.specialityMenu[1]);
+		}
+
+		TEST_METHOD(T009_mirazurcoffee3)
+		{
+			//Arrange
+			char* input = "mirazurcoffee3";
+			char* expected = "mirazurcoffee3";
+			PRESTAURANTNODE	list = loadRestaurants(TESTDIRECTORY);
+
+			//Act
+			PRESTAURANTNODE p = searchRecipe(list, input);
+
+			//Assert
+			Assert::AreEqual(expected, p->restaurant.specialityMenu[2]);
 		}
 	};
 }
